697e6817881b3ede7f7635cd76bf8c6a51e439a6
dmitry131131 <antontsev.dmitry@yandex.ru>
2023-09-15 01:10:13 +0300

    Made all color output functions

--- include/Color_output.h
+++ include/Color_output.h
@@ -5,6 +5,35 @@
 #ifndef COLOR_CONSOLE
 #define COLOR_CONSOLE
 
+#include <stdio.h>
+#include <stdarg.h>
 
+
+enum consoleColor{
+    COLOR_BLACK   = 0,
+    COLOR_RED     = 1,
+    COLOR_GREEN   = 2,
+    COLOR_YELLOW  = 3,
+    COLOR_BLUE    = 4,
+    COLOR_PURPLE  = 5,
+    COLOR_CYAN    = 6,
+    COLOR_WHITE   = 7,
+    COLOR_DEFAULT = 8
+};
+
+enum consoleStyle{
+    STYLE_NORMAL     = 0,
+    STYLE_BOLD       = 1,
+    STYLE_UNDERLINED = 4,
+    STYLE_BLINKING   = 5,
+    STYLE_INVERT_C   = 7,
+    STYLE_INVISIBLE  = 8
+};
+
+void color_printf(enum consoleColor color, enum consoleStyle style, const char* text, ...);
+
+void color_fprintf(enum consoleColor color, enum consoleStyle style, FILE* stream, const char* text, ...);
+
+void set_color(enum consoleColor color, enum consoleStyle style);
 
 #endif
--- src/Color_output.cpp
+++ src/Color_output.cpp
@@ -2,11 +2,29 @@
  * @file
  * @brief Color console output functions source
 */
-#include <stdio.h>
-#include <stdarg.h>
 #include "Color_output.h"
 
-void color_printf()
+void color_printf(enum consoleColor color, enum consoleStyle style, const char* text, ...)
 {
-    
+    va_list arguments;
+    va_start(arguments, text);
+
+    printf("\33[%d;3%dm", style, color);
+    vprintf(text, arguments);
+    printf("\33[0;39m");
+}
+
+void color_fprintf(enum consoleColor color, enum consoleStyle style, FILE* stream, const char* text, ...)
+{
+    va_list arguments;
+    va_start(arguments, text);
+
+    fprintf(stream, "\33[%d;3%dm", style, color);
+    vfprintf(stream, text, arguments);
+    fprintf(stream, "\33[0;39m");
+}
+
+void set_color(enum consoleColor color, enum consoleStyle style)
+{
+    printf("\33[%d;3%dm", style, color);
 }
